c17b88d09f62345d0bc21a4ef95f853b879c66b9
Mikndesu <mi22kknsinge@ezweb.ne.jp>
2023-12-07 19:06:52 +0900

    implement ReLU function

--- src/chapter3.rs
+++ src/chapter3.rs
@@ -20,6 +20,10 @@ fn sigmoid(x: na::RowDVector<f64>) -> na::RowDVector<f64> {
         .into()
 }
 
+fn relu(x: na::RowDVector<f64>) -> na::RowDVector<f64> {
+    x.iter().map(|&t| t.max(0.0)).collect::<Vec<f64>>().into()
+}
+
 #[test]
 fn test_step_function() {
     let x: Vec<f64> = (-500..=500)
@@ -61,6 +65,31 @@ fn test_sigmoid() {
         .x_label_area_size(30)
         .y_label_area_size(30)
         .build_cartesian_2d(-5.0f64..5.5f64, 0.0..1.3)
+        .unwrap();
+    plot.configure_mesh().draw().unwrap();
+    plot.draw_series(LineSeries::new(
+        x.iter().zip(y.iter()).map(|(x, y)| (*x, (*y).into())),
+        &RED,
+    ))
+    .unwrap();
+}
+
+#[test]
+fn test_relu() {
+    let x: Vec<f64> = (-500..=500)
+        .into_iter()
+        .map(|t| (t as f64) * 0.01)
+        .collect();
+    let y = relu(x.clone().into());
+
+    let root = BitMapBackend::new("relu.png", (640, 480)).into_drawing_area();
+    root.fill(&WHITE).unwrap();
+    let mut plot = ChartBuilder::on(&root)
+        .caption("ReLU", ("sans-serif", 30).into_font())
+        .margin(10)
+        .x_label_area_size(30)
+        .y_label_area_size(30)
+        .build_cartesian_2d(-5.0f64..5.5f64, 0.0..6.0)
         .unwrap();
     plot.configure_mesh().draw().unwrap();
     plot.draw_series(LineSeries::new(
