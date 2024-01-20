4b26b5491dc468f292f1f54218f3018a2c35781d
Mikndesu <mi22kknsinge@ezweb.ne.jp>
2024-01-21 04:20:49 +0900

    remove two_layer_net

--- two_layer_net/src/layers.rs
+++
@@ -1,9 +0,0 @@
-pub mod affine_layer;
-pub mod relu_layer;
-pub mod sigmoid_layer;
-pub mod softmax_with_loss_layer;
-
-pub trait Layer {
-    fn forwards(&mut self, x: &na::DMatrix<f64>) -> na::DMatrix<f64>;
-    fn backwards(&mut self, x: &na::DMatrix<f64>) -> na::DMatrix<f64>;
-}
--- two_layer_net/src/lib.rs
+++
@@ -1,3 +0,0 @@
-extern crate nalgebra as na;
-pub mod layers;
-pub mod two_layer_net;
